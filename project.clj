3057b417d385abf87c45282b71f9c8095d33defd
Tommi Reiman <tommi@iki.fi>
2016-03-09 23:54:55 +0200

    Use Clojure 1.8

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject metosin/schema-viz "0.1.0"
+(defproject metosin/schema-viz "0.0.1"
   :description "Schema visualization using graphviz"
   :url "https://github.com/metosin/schema-viz"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
-  :dependencies [[prismatic/schema "1.0.4"]
+  :dependencies [[prismatic/schema "1.0.5"]
                  [rhizome "0.2.5"]]
   :plugins [[funcool/codeina "0.3.0"]]
 
@@ -13,9 +13,9 @@
             :src-uri "http://github.com/metosin/schema-viz/blob/master/"
             :src-uri-prefix "#L"}
 
-  :profiles {:dev {:plugins [[jonase/eastwood "0.2.2"]]
-                   :dependencies [[criterium "0.4.3"]
-                                  [org.clojure/clojure "1.7.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-RC3"]]}}
-  :aliases {"all" ["with-profile" "dev:dev,1.8"]
+  :profiles {:dev {:plugins [[jonase/eastwood "0.2.3"]]
+                   :dependencies [[criterium "0.4.4"]
+                                  [org.clojure/clojure "1.8.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
+  :aliases {"all" ["with-profile" "dev:dev,1.7"]
             "test-clj" ["all" "do" ["test"] ["check"]]})
